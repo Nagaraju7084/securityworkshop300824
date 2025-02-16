4ba2a52e2a6d19a32014d31a8b6786fae54e05ac
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2025-02-16 16:15:40 +0530

    password encoder error
    if we not configure passwordencoder

--- src/main/java/com/medilab/preclinic/config/SecurityConfig.java
+++ src/main/java/com/medilab/preclinic/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.medilab.preclinic.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,7 +20,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.and()
 		.httpBasic(); //or through httpbasic means via postman headers
 	}
+	
+	//instead of default generated user details such as user and generated password
+	//define our custom user details may be in memory user details manager or jdbc user details manager, etc
+	//using AuthenticationManagerBuilder
+	//AuthenticationManagerBuilder will build the AuthenticationManager
+	//AuthenticationManager will be implemented by the AuthenticationProvider 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication() //this inmemoryuserdetailsmanager take all these users and calling the userdetails => createuser
+		//inMemoryAuthentication() => InMemoryUserDetailsManagerConfigurer => InMemoryUserDetailsManager => createUser
+		//createUser(UserDetails user) = given user details
+		//finally the users are added into the user map
+		.withUser("admin").password("admin").authorities("admin") //our own user store using in memory
+		.and()
+		.withUser("user").password("user").authorities("user");
+		//if we run the application, we will get the exception as : java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
+		//because AuthenticationProvider will verify the password what we are enter with the password present in UserDetailsService using PasswordEncoder
+		//this PasswordEncoder used by the AuthenticationProvider
+		//that is the reason, while authenticating, AuthenticationProvider have not found any
+		//PasswordEncoder, and hence we got the PasswordEncoder error here
+	}
 }
-
 
 //beans we can define with @Bean, those beans we can define in the class which annotated with @Configuration
